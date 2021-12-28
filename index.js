fc1bb48b9864ac0eaa842369d10ec46ce2cbeaa2
dvlprwchoi <dvlpr.wchoi@gmail.com>
2021-12-27 18:03:24 -0600

    Add cors

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
-require('dotenv').config();
 const express = require('express');
+const cors = require('cors');
 const app = express();
 const mongoose = require('mongoose');
+
+require('dotenv').config();
 const PORT = process.env.PORT;
 const MONGODB_URL = process.env.MONGODB_URL;
 
 mongoose.connect(MONGODB_URL);
+
+app.use(cors());
 
 app.listen(PORT, () => {
   console.log(`✅!!!SERVER IS RUNNING ON PORT:${PORT}!!!✅`);
