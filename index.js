38cd965f591aea4020ff15256d2d8972f997b5ad
dvlprwchoi <dvlpr.wchoi@gmail.com>
2021-12-27 18:49:24 -0600

    Add mongoose

--- index.js
+++ index.js
@@ -1,20 +1,64 @@
+//=============================================================================
+// Basic Config
+//=============================================================================
 const express = require('express');
 const cors = require('cors');
 const app = express();
+
+//=============================================================================
+// Mongo Atlas Connection
+//=============================================================================
+require('dotenv').config();
 const mongoose = require('mongoose');
 
-require('dotenv').config();
+// Mongo URL and Connection
 const PORT = process.env.PORT;
 const MONGODB_URL = process.env.MONGODB_URL;
+// const db = mongoose.connection;
 
-mongoose.connect(MONGODB_URL);
+// Connect to Mongo
 
+// mongoose.connect(MONGODB_URL, {
+//   useNewUrlParser: true,
+//   useUnifiedTopology: true,
+// });
+
+// Connection Error/Success - optional but can be helpful
+// Define callback functions for various events
+// db.on('error', (err) => console.log(err.message + ' is Mongod not running?'));
+// db.on('connected', () => console.log('mongo connected at: ', MONGODB_URL));
+// db.on('disconnected', () => console.log('mongo disconnected'));
+
+// Open the Connection
+// db.on('open', () => {
+//   console.log('✅ mongo connection made!');
+// });
+
+main().catch((err) => console.log(err));
+
+async function main() {
+  await mongoose.connect(MONGODB_URL);
+}
+
+//=============================================================================
+// Middleware
+//=============================================================================
 app.use(cors());
+app.use(express.json());
 
+//
 app.post('/create', (req, res) => {
-  res.send('Created!!');
+  const { username, password } = req.body;
+  res.json({
+    username,
+    password,
+  });
+  //   res.send('Created!!');
 });
 
+//=============================================================================
+// START SERVER
+//=============================================================================
 app.listen(PORT, () => {
   console.log(`✅!!!SERVER IS RUNNING ON PORT:${PORT}!!!✅`);
 });
